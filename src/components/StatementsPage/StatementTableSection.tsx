04d45d2b4e8d4994321bd67ec226bfb583293d7c
aldentantan <aldentanfk@gmail.com>
2025-12-20 13:10:24 +0800

    Extract TableSection, add empty and loading states for StatementTable component

---
+++ src/components/StatementsPage/StatementTableSection.tsx
@@ -0,0 +1,80 @@
+import { Section } from "../Section";
+import { Box, Table, TableCell, TableHead, TableContainer, TableRow, TableBody, Paper, IconButton, Tooltip, Typography, CircularProgress } from "@mui/material";
+import { formatDate } from '../../utils/formatDate'
+import type { Statement } from "../../types";
+
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import ListRoundedIcon from '@mui/icons-material/ListRounded';
+
+interface StatementTableSectionProps {
+  statements: Statement[];
+  viewTripSummary: (statementId: string) => void;
+  viewPdf: (statementId: string) => void;
+  deleteRow: (statementId: string) => void;
+  loadingSummary: boolean;
+  loadingStatementId?: string;
+}
+
+export const StatementTableSection = ({ statements, viewTripSummary, viewPdf, deleteRow, loadingSummary, loadingStatementId }: StatementTableSectionProps) => {
+  if (statements.length == 0) {
+    return (
+      <Section>
+        <Typography variant="h2" sx={{ mt: 5 }}>
+          No statements uploaded yet.
+        </Typography>
+      </Section>
+    )
+  }
+
+  return (
+    <Section>
+      <TableContainer component={Paper}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Month</TableCell>
+              <TableCell>File Name</TableCell>
+              <TableCell>Date Uploaded</TableCell>
+              <TableCell>Total Fares Paid</TableCell>
+              <TableCell>Journeys</TableCell>
+              <TableCell align="right">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {statements.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.statement_month}</TableCell>
+                <TableCell>{row.file_name}</TableCell>
+                <TableCell>{formatDate(row.created_at)}</TableCell>
+                <TableCell>${row.total_fare}</TableCell>
+                <TableCell>{row.journey_count}</TableCell>
+                <TableCell>
+                  <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: 1 }}>
+                    <Tooltip title="View Trip Summary" placement="top">
+                      {loadingSummary && loadingStatementId === String(row.id) ? (
+                        <CircularProgress size={24} />
+                      ) : (
+                        <IconButton onClick={() => viewTripSummary(String(row.id))} disabled={loadingSummary}><ListRoundedIcon /></IconButton>
+                      )}
+                    </Tooltip>
+                    <Tooltip title="View SimplyGo PDF" placement="top">
+                      <IconButton onClick={() => viewPdf(String(row.id))} disabled={loadingSummary}><OpenInNewIcon /></IconButton>
+                    </Tooltip>
+                    <Tooltip title="Retrigger Calculation for Statement" placement="top">
+                      <IconButton disabled={loadingSummary}><AutorenewIcon /></IconButton>
+                    </Tooltip>
+                    <Tooltip title="Delete Statement" placement="top">
+                      <IconButton color="error" onClick={() => deleteRow(String(row.id))} disabled={loadingSummary}><DeleteRoundedIcon /></IconButton>
+                    </Tooltip>
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Section>
+  )
+}
--- src/pages/StatementsPage.tsx
+++ src/pages/StatementsPage.tsx
@@ -1,39 +1,37 @@
 import { SectionHeader } from "../components/SectionHeader";
-import { Section } from "../components/Section";
-import { Box, Typography, Button, Table, TableCell, TableHead, TableContainer, TableRow, TableBody, Paper, IconButton, Tooltip } from "@mui/material";
-import FileUploadRoundedIcon from '@mui/icons-material/FileUploadRounded';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import AutorenewIcon from '@mui/icons-material/Autorenew';
-import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
-import ListRoundedIcon from '@mui/icons-material/ListRounded';
+import { Box, Typography, Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { fetchStatements, viewStatementTripSummary, createSignedLink, deleteStatement } from "../services/statementsService";
-import { formatDate } from '../utils/formatDate'
 import { useNavigate } from "react-router-dom";
 import { useJourneyContext } from "../contexts/JourneyContext";
-
+import { StatementTableSection } from "../components/StatementsPage/StatementTableSection";
+import FileUploadRoundedIcon from '@mui/icons-material/FileUploadRounded';
 import type { Statement } from "../types";
 
 export default function StatementsPage() {
   const [statements, setStatements] = useState<Statement[]>([]);
+  const [loadingSummary, setLoadingSummary] = useState<boolean>(false);
+  const [loadingStatementId, setLoadingStatementId] = useState<string>();
   const { setJourneys, setFares } = useJourneyContext();
   const navigate = useNavigate();
 
   useEffect(() => {
     async function loadStatements() {
-      console.log("Fetching statements...");
       const res = await fetchStatements();
       setStatements(res);
-      console.log("Statements loaded:", res);
     }
     loadStatements();
   }, []);
 
   const viewTripSummary = async (statementId: string) => {
+    setLoadingSummary(true);
+    setLoadingStatementId(statementId);
     const { journeys, fares } = await viewStatementTripSummary(statementId);
     setJourneys(journeys);
     setFares(fares);
-    navigate('/trip-summary')
+    navigate('/trip-summary');
+    setLoadingSummary(false);
+    setLoadingStatementId(undefined);
   }
 
   const viewPdf = async (statementId: string) => {
@@ -60,49 +58,7 @@ export default function StatementsPage() {
           Upload New
         </Button>
       </Box>
-      <Section>
-        <TableContainer component={Paper}>
-          <Table size="small">
-            <TableHead>
-              <TableRow>
-                <TableCell>Month</TableCell>
-                <TableCell>File Name</TableCell>
-                <TableCell>Date Uploaded</TableCell>
-                <TableCell>Total Fares Paid</TableCell>
-                <TableCell>Journeys</TableCell>
-                <TableCell align="right">Actions</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {statements.map((row) => (
-                <TableRow key={row.id}>
-                  <TableCell>{row.statement_month}</TableCell>
-                  <TableCell>{row.file_name}</TableCell>
-                  <TableCell>{formatDate(row.created_at)}</TableCell>
-                  <TableCell>${row.total_fare}</TableCell>
-                  <TableCell>{row.journey_count}</TableCell>
-                  <TableCell>
-                    <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: 1 }}>
-                      <Tooltip title="View Trip Summary" placement="top">
-                        <IconButton onClick={() => viewTripSummary(String(row.id))}><ListRoundedIcon /></IconButton>
-                      </Tooltip>
-                      <Tooltip title="View SimplyGo PDF" placement="top">
-                        <IconButton onClick={() => viewPdf(String(row.id))}><OpenInNewIcon /></IconButton>
-                      </Tooltip>
-                      <Tooltip title="Retrigger Calculation for Statement" placement="top">
-                        <IconButton><AutorenewIcon /></IconButton>
-                      </Tooltip>
-                      <Tooltip title="Delete Statement" placement="top">
-                        <IconButton color="error" onClick={() => deleteRow(String(row.id))}><DeleteRoundedIcon /></IconButton>
-                      </Tooltip>
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Section>
+      <StatementTableSection statements={statements} viewTripSummary={viewTripSummary} viewPdf={viewPdf} deleteRow={deleteRow} loadingSummary={loadingSummary} loadingStatementId={loadingStatementId}/>
     </Box>
   )
 }
